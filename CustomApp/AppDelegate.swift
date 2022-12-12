e37fcf6b5b704b75def1e39adf23552302be2609
Devkaran Makvana <devkaran.makvana@gtcsys.com>
2022-12-12 17:01:57 +0530

    inital commit

--- CustomApp/AppDelegate.swift
+++ CustomApp/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print(Bundle.main.infoDictionary?["BASE_URL"])
+//        print(Config.current)
         return true
     }
 
--- CustomApp/ViewController.swift
+++ CustomApp/ViewController.swift
@@ -4,16 +4,48 @@
 //
 //  Created by Devkaran's Mac mini on 07/12/22.
 //
-
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let url = Bundle.main.object(forInfoDictionaryKey: "API_URL") {
+            print("Current url: \(url)")
+        }
+        
+        if let bundleId = Bundle.main.object(forInfoDictionaryKey: "BUNDLE_IDENTIFIER") {
+            print("Current Bundle: \(bundleId)")
+        }
+        
+        let titleLabel = {
+            let label = UILabel(frame: CGRect(x: 0, y:0, width: 200, height: 60))
+            label.backgroundColor = .red
+            if let configurationTitle = Bundle.main.infoDictionary?["BASE_URL"]  {
+                label.text = configurationTitle as! String
+            }
+            label.textColor = .white
+            label.textAlignment = .center
+            return label
+        }()
+        view.addSubview(titleLabel)
+        titleLabel.center = view.center
+        
+        if let color = Bundle.main.infoDictionary?["BACK_COLOR"]  {
+            print(color)
+            switch color as! String {
+                case "red":
+                view.backgroundColor = UIColor.red //or you can use hex colors here
+                case "blue":
+                view.backgroundColor = UIColor.blue
+                case "gray":
+                view.backgroundColor = UIColor.gray
+                case "green":
+                view.backgroundColor = UIColor.green
+                case "yellow":
+                view.backgroundColor = UIColor.yellow
+                default :
+                view.backgroundColor = UIColor.black
+                }
+        }
     }
-
-
 }
-
